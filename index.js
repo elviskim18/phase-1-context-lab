f1d26a54a583186e0ed9a463be205a79f856c5ed
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-19 19:04:07 +0300

    commit tree 340d5b8023c3cebd81a85959f5c50471b856fef6

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 /* Your Code Here */
+let createEmployeeRecord = row=>{
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+let createEmployeeRecords = employeeRowData=> {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+let createTimeInEvent = dateStamp=>{
+    let [date, hour] = dateStamp.split(' ')
 
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let createTimeOutEvent = dateStamp=>{
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let hoursWorkedOnDate = dateSort=>{
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+let wagesEarnedOnDate = dateSort=>{
+    let rawWage = hoursWorkedOnDate.call(this, dateSort)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
